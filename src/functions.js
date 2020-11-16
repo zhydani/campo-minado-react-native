4e24816bcd4ddf7673456a3e67ba1cf241e5d7bc
zhydani <zhydani@gmail.com>
2020-11-16 17:21:15 -0300

    adicionando funcoes para preparar o campo minado

---
+++ src/functions.js
@@ -0,0 +1,42 @@
+const { parse } = require("@babel/core")
+const { default: params } = require("./params")
+
+// criando uma matriz de objetos com todas as funcoes definidas na lógica do jogo
+const createBoard = (rows, columns) => {
+    return Array(rows).fill(0).map((_, row) => {
+        return Array(columns).fill(0).map((_, column) => {
+            return {
+                row,
+                column,
+                opened: false,
+                flagged: false,
+                mined: false,
+                exploded: false,
+                nearMines: 0
+            }
+        })
+    })
+}
+// espalhando as minas de forma randomica
+const spreadMines = (board, minesAmount) => {
+    const rows = board.length
+    const columns = board[0].length
+    let minesPlanted = 0
+    while (minesPlanted < minesAmount){
+        const rowSel = parseInt(Math.random() * rows, 10)
+        const columnSel = parseInt(Math.random() * columns, 10)
+
+        if (!board[rowSel][columnSel].mined){
+            board[rowSel][columnSel].mined = true
+            minesPlanted++
+        }
+    }
+}
+
+const createMinedBoard = (rows, columns, minesAmount) => {
+    const board = createBoard(rows, columns)
+    spreadMines(board, minesAmount)
+    return board
+}
+
+export { createMinedBoard }
