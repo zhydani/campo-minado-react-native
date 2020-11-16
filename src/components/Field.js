cc112381d564af0a929364f34b1c8ca2b3b3a06e
zhydani <zhydani@gmail.com>
2020-11-16 15:09:04 -0300

    bandeirinha adicionada

--- src/components/Field.js
+++ src/components/Field.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import params from '../params';
 import Mine from './Mine'
+import Flag from './Flag'
 
 const Field = props => {
 
-    const { mined, opened, nearMines, exploded } = props
+    const { mined, opened, nearMines, exploded, flagged } = props
 
     const styleField = [styles.field]
     if (opened) styleField.push(styles.opened)
     if (exploded) styleField.push(styles.exploded)
-    if (styleField.length === 1) styleField.push(styles.regular)
+    if (flagged) styleField.push(styles.flagged)
+    if (!opened && !exploded) styleField.push(styles.regular)
 
     let color = null
     if (nearMines > 0) {
@@ -30,6 +32,7 @@ const Field = props => {
             </Text> : false 
             }
             {mined && opened ? <Mine /> : false}
+            {flagged && !opened ? <Flag /> : false} 
         </View>
     )
 };
