29cf03ae985aac8462e618a1e4089916fc6c5ac9
zhydani <zhydani@gmail.com>
2020-11-16 14:15:04 -0300

    adicionando o estilo do campo

---
+++ src/components/Field.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import params from '../params';
+
+const Field = props => {
+    const styleField = [styles.field]
+    // outros estilos aqui
+    if (styleField.length === 1) styleField.push(styles.regular)
+
+    return (
+        <View style={styleField}>
+
+        </View>
+    )
+};
+
+const styles =  StyleSheet.create({
+    field: {
+        height: params.blockSize,
+        width: params.blockSize,
+        borderWidth: params.borderSize
+    },
+    regular: {
+        backgroundColor: '#999',
+        borderLeftColor: '#CCC',
+        borderTopColor: '#CCC',
+        borderRightColor: '#333',
+        borderBottomColor: '#333'
+    }
+});
+
+export default Field;
